e159c279e0f05dd245bd174f2e6ca8e21fefefe9
Partha Rajendra <pvraj011@gmail.com>
2013-08-19 15:55:52 -0400

    page for services
    currently blank; most likely will add html for bootstrap dropdown

---
+++ wp-content/themes/blackbird/services.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: Services page
+*/
+?>
+
+<?php get_header(); ?>
+<div class="page-heading">
+    <h1 class="page-title"><?php the_title(); ?></h1>
+    <div class="clear"></div>
+</div>
+<!--Start Page Content -->
+    <link href="/wp-content/themes/blackbird/css/bootstrap.min.css" type="text/css" rel="stylesheet"> 
+    <link href="/wp-content/themes/blackbird/css/exec-styles.css" type="text/css" rel="stylesheet">
+    <script src="/wp-content/themes/blackbird/js/jquery-1.10.2.min.js"></script>
+    <script src="/wp-content/themes/blackbird/js/bootstrap.min.js"></script>
+<div class="page-content-container">
+    <div class="page-content">
+        <div class="container">
+            
+
+        </div>
+
+    </div>
+</div>
+</div>
+</div>
+</div>
+<?php get_footer(); ?>

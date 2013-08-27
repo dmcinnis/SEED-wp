3cf6533218b54234d48ceaf35da9e41f026ce3ed
dmcinnis <apmcinnis@gmail.com>
2013-08-26 23:25:31 -0400

    first iteration of clients page; still need to add popovers containing additional client details

---
+++ wp-content/themes/blackbird/clients.php
@@ -0,0 +1,178 @@
+<?php
+/*
+Template Name: Clients page
+*/
+?>
+
+<?php get_header(); ?>
+<div class="page-heading">
+    <h1 class="page-title"><?php the_title(); ?></h1>
+    <div class="clear"></div>
+</div>
+<!--Start Page Content -->
+    <link href="/wp-content/themes/blackbird/css/bootstrap.min.css" type="text/css" rel="stylesheet"> 
+    <link href="/wp-content/themes/blackbird/css/client-styles.css" type="text/css" rel="stylesheet">
+    <script src="/wp-content/themes/blackbird/js/jquery-1.10.2.min.js"></script>
+    <script src="/wp-content/themes/blackbird/js/bootstrap.min.js"></script>
+
+<div class="page-content-container">
+    <div class="page-content">
+      <ul class="client" id="tri">
+        <li class="client">
+          <h1>The Americas</h1>
+            <h3>Chile</h3>
+              VE Global - (Fall 2011)<br>
+              VE Global - (Spring 2011)<br>
+              VE Global - (Fall 2010)<br>
+              VE Global Operations - (Fall 2010)<br>
+            <h3>Costa Rica</h3>
+              DoGood - (Fall 2012)<br>
+              Plantech - (Fall 2012)<br>
+            <h3>Dominican Republic</h3>
+              Chaka Marketbridge - (Fall 2012)<br>
+              Chaka MarketBridge: B2B Marketing and Strategic Partnerships - (Spring 2012)<br>
+              Chaka MarketBridge: Coffee Market Research - (Spring 2012)<br>
+            <h3>Ecuador</h3>
+              Rebranding Community Ventures International - (Spring 2009)<br>
+              Amor y Esperanza Financial/Operational Sustainability - (Fall 2008)<br>
+              Assess Yuyani Tours - (Fall 2008)<br>
+              Rebranding Community Ventures International - (Fall 2008)<br>
+            <h3>Guatemala</h3>
+              Ixtatan Microfinance - (Fall 2009)<br>
+              Ixtatan Restaurant - (Fall 2009)<br>
+            <h3>Haiti</h3>
+              Haiti Street - (Fall 2011)<br>
+              Building Haiti - (Spring 2010)<br>
+            <h3>Honduras</h3>
+              Global Microfinance Brigades (operations) - (Spring 2011)<br>
+              Global Microfinance Brigades (operations) - (Fall 2010)<br>
+              La Ceiba Microfinance - (Fall 2008)<br>
+            <h3>Mexico</h3>
+              Forjadores de Negocios - (Fall 2012)<br>
+              Forjadores Microfinance - (Spring 2012)<br>
+            <h3>Nicaragua</h3>
+              Chaka Marketbridge - (Fall 2012)<br>
+              Chaka MarketBridge: B2B Marketing and Strategic Partnerships - (Spring 2012)<br>
+              Chaka MarketBridge: Coffee Market Research - (Spring 2012)<br>
+              Chaka Marketbridge - (Fall 2011)<br>
+              Chaka Marketbridge - (Spring 2011)<br>
+              Chaka Marketbridge - (Fall 2010)<br>
+              Chaka MarketBridge (Entry Strategies) - (Fall 2010)<br>
+              The Bluefields Project - (Spring 2010)<br>
+              The Bluefields Project - (Fall 2009)<br>
+              The Bluefields Project - (Spring 2009)<br>
+            <h3>United States</h3>
+              Bank On Greater Charlottesville - (Fall 2012)<br>
+              Birthing Project USA - (Fall 2012)<br>
+              Giv2Giv - (Fall 2012)<br>
+              Nouri - (Fall 2012)<br>
+              Autism Society of Indiana - (Spring 2012)<br>
+              Chaka MarketBridge: B2B Marketing and Strategic Partnerships - (Spring 2012)<br>
+              Chaka MarketBridge: Coffee Market Research - (Spring 2012)<br>
+              Charlottesville Social Business (Catering) - (Spring 2012)<br>
+              FAN4Kids - (Spring 2012)<br>
+              ImpactAssets - (Spring 2012)<br>
+              Metrics & Evaluation of Charlottesville Nonprofits - (Spring 2012)<br>
+              National Consumers League - (Spring 2012)<br>
+              National Utilities Diversity Council - (Spring 2012)<br>
+              Native American Children’s Alliance - (Spring 2012)<br>
+              New Hope Children’s Clinic - (Spring 2012)<br>
+              Sexual Assault Resource Agency (SARA) - (Spring 2012)<br>
+              Touchstones Discussion Project - (Spring 2012)<br>
+              Tyrrell County Community Development Corporation - (Spring 2012)<br>
+              All About Animals Rescue (Outcome Management) - (Fall 2011)<br>
+              C.A.F.E. (Marketing Strategy) - (Fall 2011)<br>
+              Fan4Kids (Strategic Planning) - (Fall 2011)<br>
+              Freedom to Fly – Operations - (Fall 2011)<br>
+              Freedom to Fly – Strategic Partnerships - (Fall 2011)<br>
+              KaleidoLINKS (Fund Development) - (Fall 2011)<br>
+              Touchstones - (Fall 2011)<br>
+              Hope Consulting (operations) - (Spring 2011)<br>
+              Initiative reCOVER (marketing) - (Spring 2011)<br>
+              Hope Community (Consulting) - (Fall 2010)<br>
+              Hope Community (Marketing) - (Fall 2010)<br>
+              Hope Food Rescue - (Fall 2010)<br>
+              Hope Wedding - (Fall 2010)<br>
+              Hope Community (Consulting) - (Spring 2010)<br>
+              Hope Community (Marketing) - (Spring 2010)<br>
+              Hope Food Rescue - (Spring 2010)<br>
+              Street Soccer UVA - (Spring 2010)<br>
+              Hope Cooks! - (Fall 2009)<br>
+              Hope Food Rescue - (Fall 2009)<br>
+              Street Soccer UVA - (Fall 2009)<br>
+              Initiative reCOVER - (Spring 2009)<br>
+              MIFOS Initiative - (Fall 2008)<br>
+        </li>
+        <li class="client">
+          <h1>Eurasia</h1>
+            <h3>Afghanistan</h3>
+              PROMFA - (Fall 2011)<br>
+            <h3>Cambodia</h3>
+              Bloom Design - (Spring 2009)<br>
+              Bloom Operations - (Spring 2009)
+            <h3>China</h3>
+              e8 - (Fall 2012)<br>
+            <h3>India</h3>
+              Profugo - (Fall 2012)<br>
+              Inspiring Lives Brand Development - (Spring 2009)<br>
+            <h3>Mongolia</h3>
+              Mongolia Felt - (Fall 2011)<br>
+            <h3>Nepal</h3>
+              Nepal -- Farming - (Fall 2011)<br>
+              Nepal -- Jewelry - (Fall 2011)<br>
+            <h3>Thailand</h3>
+              Chaka Marketbridge - (Fall 2011)<br>
+              Chaka Marketbridge - (Spring 2011)<br>
+              Chaka Marketbridge - (Fall 2010)<br>
+            <h3>Yemen</h3>
+              Yemeni Honey Cooperative (marketing) - (Spring 2011)<br>
+              Yemeni Honey Cooperative (marketing) - (Fall 2010)<br>
+        </li>
+        <li class="client">
+          <h1>Africa</h1>
+            <h3>Ethiopia</h3>
+              Link Ethiopia Branding - (Spring 2009)<br>
+              Link Ethiopia Microfinance - (Spring 2009)<br>
+            <h3>Ghana</h3>
+              Amandla Awethu Africa Microfinance - (Spring 2010)<br>
+              Amandla Awethu Africa Microfinance - (Fall 2009)<br>
+            <h3>Kenya</h3>
+              Kilifi Kids - (Fall 2011)<br>
+              Amani Ya Juu (marketing) - (Spring 2011)<br>
+              Amani Ya Juu (sales evaluation) - (Spring 2011)<br>
+            <h3>Nigeria</h3>
+              Leadership Initiatives – Sister School Program (operations) - (Spring 2011)<br>
+              Leadership Initiatives Community Expansion - (Fall 2010)<br>
+              Leadership Initiatives Corporate Sponsors - (Fall 2010)<br>
+              Leadership Initiatives - (Spring 2010)<br>
+              Leadership Initiatives - (Fall 2009)<br>
+              Leadership Initiatives - (Spring 2009)<br>
+            <h3>Sierra Leone</h3>
+              Sierra Leone Women’s Center - (Spring 2009)<br>
+            <h3>Tanzania</h3>
+              Global Art Partners (marketing & research) - (Spring 2011)<br>
+            <h3>Togo</h3>
+              Amandla Awethu Africa Farming - (Spring 2010)<br>
+              Amandla Awethu Africa Farming - (Fall 2009)<br>
+            <h3>Zimbabwe</h3>
+              Cable Fin - (Fall 2012)<br>
+              Mentoring Zimbabwe - (Fall 2012)<br>
+              Seeds of Africa (market research) - (Spring 2011)<br>
+        </li>
+        <li class="client">
+          <h1>Oceania</h1>
+            <h3>Papua New Guinea</h3>
+              PNG Wind Powered Water Utility - (Fall 2011)<br>
+        </li>
+        <li class="client">
+          <h1>No Region/Country</h1>
+            5 for Fairness (Marketing Strategy) - (Fall 2011)<br>
+            Microfinance Preparedness Assessment & Strategy Development - (Fall 2008)<br>
+        </li>
+      </ul>
+    </div>
+</div>
+</div>
+</div>
+</div>
+<?php get_footer(); ?>
